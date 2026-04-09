a6944f800e8ab6054065328e25f3674904643fde
Manifesto141003 <gunawanirsjad19@gmail.com>
2026-04-09 14:17:54 +0700

    Update Monthly

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -23,7 +23,7 @@ export const Home = () => {
       const progress = step / steps;
 
       setCounts({
-        aum: Math.floor(6 * progress),
+        aum: Math.floor(7 * progress),
         clients: Math.floor(10 * progress),
         return: (18.15 * progress).toFixed(1),
         year: 2025,
