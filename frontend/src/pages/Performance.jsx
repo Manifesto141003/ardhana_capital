bef6a6085264cb8b129d259e3c5d8ce43b25fdac
Manifesto141003 <gunawanirsjad19@gmail.com>
2026-06-20 18:05:48 +0700

    Update Monthly

--- frontend/src/pages/Performance.jsx
+++ frontend/src/pages/Performance.jsx
@@ -105,13 +105,13 @@ export const Performance = () => {
     { title: 'Quarter I',   period: 'Aug – Oct 2025',      return: 29.4,  benchmark: 29.4,  outperformance: 0, status: 'done'        },
     { title: 'Quarter II',  period: 'Nov 2025 – Jan 2026', return: 6.79,  benchmark: 6.79,  outperformance: 0, status: 'done'        },
     { title: 'Quarter III', period: 'Feb – Apr 2026',      return: 3.66, benchmark: 3.66, outperformance: 0, status: 'done'     },
-    { title: 'Quarter IV',  period: 'May – Jul 2026',      return: 3.56     benchmark: 3.56,     outperformance: 0, status: 'ongoing' },
+    { title: 'Quarter IV',  period: 'May – Jul 2026',      return: 3.56, benchmark: 3.56,     outperformance: 0, status: 'ongoing' },
     { title: 'Quarter V',   period: 'Aug – Oct 2026',      return: 0,     benchmark: 0,     outperformance: 0, status: 'Not Started' },
   ];
 
   const yearlyData = [
     { year: '2025', return: 26.09, benchmark: 26.09, status: 'done'    },
-    { year: '2026', return: 1.50, benchmark: 28.86, status: 'ongoing' },
+    { year: '2026', return: 17.64, benchmark: 28.86, status: 'ongoing' },
   ];
 
   const data = viewMode === 'quarterly' ? quarterlyData : yearlyData;
