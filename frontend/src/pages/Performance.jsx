09708aab1ab60b42dbb96b25c71278d7ee74375e
Manifesto141003 <gunawanirsjad19@gmail.com>
2026-08-10 10:19:48 +0700

    Initial commit

--- frontend/src/pages/Performance.jsx
+++ frontend/src/pages/Performance.jsx
@@ -107,7 +107,7 @@ export const Performance = () => {
     { title: 'Quarter II',  period: 'Nov 2025 – Jan 2026', return: 6.79,  benchmark: 6.79,  outperformance: 0, status: 'done'        },
     { title: 'Quarter III', period: 'Feb – Apr 2026',      return: 3.66, benchmark: 3.66, outperformance: 0, status: 'done'     },
     { title: 'Quarter IV',  period: 'May – Jul 2026',      return: 3.42, benchmark: 3.42,     outperformance: 0, status: 'done' },
-    { title: 'Quarter V',  period: 'Aug – Oct 2026',      return: 10.28, benchmark: 10.28,     outperformance: 0, status: 'ongoing' },
+    { title: 'Quarter V',  period: 'Aug – Oct 2026',      return: 10.28, benchmark: 29.4,     outperformance: 0, status: 'ongoing' },
   ];
 
   const yearlyData = [
